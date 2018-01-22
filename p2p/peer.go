8de901fe37b3829acce3f8321b3c47bbd4be133a
theochupp <tclchiam@gmail.com>
2018-01-21 23:27:15 -0500

    Added string method for Peer

--- p2p/peer.go
+++ p2p/peer.go
@@ -1,12 +1,18 @@
 package p2p
 
 import (
+	"fmt"
+
 	"github.com/tclchiam/block_n_go/blockchain/entity"
 )
 
 type Peer struct {
 	Address string
 	Head    *entity.Hash
+}
+
+func (p *Peer) String() string {
+	return fmt.Sprintf("{Address: %s, Head: %s}", p.Address, p.Head)
 }
 
 type Peers []*Peer
