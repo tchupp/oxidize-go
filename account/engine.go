9a5a87c842e4205fa79a40129e14e9bd38b877a4
theochupp <tclchiam@gmail.com>
2018-02-25 13:03:05 -0500

    Moved the block processing out of the account.Engine struct

--- account/engine.go
+++ account/engine.go
@@ -1,8 +1,9 @@
 package account
 
 import (
-	"sync"
+	"io"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/tclchiam/oxidize-go/account/utxo"
 	"github.com/tclchiam/oxidize-go/blockchain"
 	"github.com/tclchiam/oxidize-go/blockchain/entity"
@@ -19,23 +20,21 @@ type Engine interface {
 }
 
 type engine struct {
-	bc   blockchain.Blockchain
-	sub  blockchain.Subscription
-	lock sync.RWMutex
-	repo *accountRepo
+	bc        blockchain.Blockchain
+	repo      *accountRepo
+	processor *blockProcessor
 }
 
 func NewEngine(bc blockchain.Blockchain) Engine {
-	channel := make(chan blockchain.Event, 10)
-	engine := &engine{
-		bc:   bc,
-		sub:  bc.Subscribe(channel),
-		repo: NewAccountRepository(),
+	repo := NewAccountRepository()
+	processor := NewBlockProcessor(bc, repo)
+	processor.Start()
+
+	return &engine{
+		bc:        bc,
+		repo:      repo,
+		processor: processor,
 	}
-
-	go engine.indexChain(channel)
-
-	return engine
 }
 
 func (e *engine) Balance(address *identity.Address) (*Account, error) {
@@ -43,9 +42,6 @@ func (e *engine) Balance(address *identity.Address) (*Account, error) {
 }
 
 func (e *engine) Transactions(address *identity.Address) (Transactions, error) {
-	e.lock.RLock()
-	defer e.lock.RUnlock()
-
 	account, err := e.repo.Account(address)
 	if err != nil {
 		return nil, err
@@ -67,66 +63,13 @@ func (e *engine) Send(spender *identity.Identity, receiver *identity.Address, ex
 }
 
 func (e *engine) Close() error {
-	e.sub.Unsubscribe()
-	return e.bc.Close()
-}
+	closers := []io.Closer{e.bc, e.processor}
 
-func (e *engine) indexChain(c <-chan blockchain.Event) {
-	var (
-		currentIndex = uint64(0)
-	)
-
-	for {
-		log.Debugf("resuming indexing with index '%d'...", currentIndex)
-
-		currentIndex = e.processNewBlocks(currentIndex)
-
-		log.Debugf("caught up with index '%d', waiting for event...", currentIndex)
-		waitForBlockEvent(c)
-	}
-}
-
-func (e *engine) processNewBlocks(currentIndex uint64) uint64 {
-	e.lock.Lock()
-	defer e.lock.Unlock()
-
-	bestBlock, err := e.bc.BestBlock()
-	for ; err == nil && currentIndex <= bestBlock.Index(); currentIndex += 1 {
-		block, err := e.bc.BlockByIndex(currentIndex)
-		if err != nil {
-			log.Panicf("error reading block for indexing: %s")
-		}
-
-		for _, tx := range block.Transactions() {
-			var spenderAddress *identity.Address
-			if len(tx.Inputs) > 0 {
-				spenderAddress = identity.FromPublicKey(tx.Inputs[0].PublicKey)
-			}
-
-			for _, out := range tx.Outputs {
-				accountTx := &Transaction{
-					amount:   out.Value,
-					spender:  spenderAddress,
-					receiver: identity.FromPublicKeyHash(out.PublicKeyHash),
-				}
-
-				e.repo.SaveTx(accountTx.spender, accountTx)
-				e.repo.SaveTx(accountTx.receiver, accountTx)
-			}
+	var result *multierror.Error
+	for _, closer := range closers {
+		if err := closer.Close(); err != nil {
+			result = multierror.Append(result, err)
 		}
 	}
-
-	return currentIndex
-}
-
-func waitForBlockEvent(c <-chan blockchain.Event) {
-	for event, ok := <-c; ; {
-		if !ok {
-			log.Debug("indexing finished!")
-			return
-		}
-		if event == blockchain.BlockSaved {
-			return
-		}
-	}
+	return result.ErrorOrNil()
 }
